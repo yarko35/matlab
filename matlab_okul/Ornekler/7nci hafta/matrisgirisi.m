83fc172129689925ebc1dbaaf2094e6646931874
yarko35 <yarkinokular@gmail.com>
2020-12-09 02:01:26 +0300

    saat 0201

--- matlab_okul/Ornekler/7nci hafta/matrisgirisi.m
+++ matlab_okul/Ornekler/7nci hafta/matrisgirisi.m
@@ -1,15 +1,18 @@
 clc; clear; close all;
 % klavyeden girilen matrise ait satır sütün sayısına göre matris oluşturma
-satir=input('Satir sayisini giriniz:')%sayı istediğimiz için varsayılan değer zaten integer(sayı)
-sutun=input('Sütun sayısını giriniz')%sütun sayısı
+satir = input('Satir sayisini giriniz:')%sayı istediğimiz için varsayılan değer zaten integer(sayı)
+sutun = input('Sutun sayisini giriniz')%sütun sayısı
 %default olarak 1 artacağı için ekleme yapmıyorum ekstradan
 for i = 1:satir%1den satir sayisina kadar devam edecek
-    for j = 1:sutun%1den sutun sayısına kadar çalışacak 
-        fprintf('Olusturulacak matrisin %d. satır %d. sutun elemanını giriniz \n',i,j);
-        matris(i,j)=input('');
-        
+
+    for j = 1:sutun%1den sutun sayısına kadar çalışacak
+        %sütünlara girilecek sayilari istedigimizin bilgisini veriyoruz kullanıcıya
+        fprintf('Olusturulacak matrisin %d. satır %d. sutun elemanını giriniz \n', i, j);
+        %sütünlara girilecek sayilari işliyoruz 
+        matris(i, j) = input(''); 
     end
-    
+
 end
+
 disp('Olusturdugunuz matris:')
 matris
