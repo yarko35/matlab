1e66f88ede921179a57fe8b051c9cd84b0d2ee24
yarko35 <yarkinokular@gmail.com>
2021-01-02 02:05:02 +0300

    düzeltmeler yapıldı

--- Final/Fonksiyon bulma/notOrtalamaHesaplayici.m
+++ Final/Fonksiyon bulma/notOrtalamaHesaplayici.m
@@ -3,12 +3,12 @@ while true
    ortalamaHesapla=input('Normal ortalam için 1 i 50-50 için 2 i tuþlayýnýz,çýkmak için  q a basýnýz: ');
 if ortalamaHesapla == 1
     vize=input('Vize notunuzu giriniz: ');
-    final=input('Final notunuzu giriniz');
+    final=input('Final notunuzu giriniz: ');
     notOrtalama=normal(vize,final);
     fprintf('Dönem sonu ortalamanýz: %d',notOrtalama);
     elseif ortalamaHesapla == 2
     vize=input('Vize notunuzu giriniz: ');
-    final=input('Final notunuzu giriniz');
+    final=input('Final notunuzu giriniz: ');
     notOrtalama=yuzdeelli(vize,final);
     fprintf('Dönem sonu ortalamanýz: %d',notOrtalama);
 elseif ortalamaHesapla == 'q'
