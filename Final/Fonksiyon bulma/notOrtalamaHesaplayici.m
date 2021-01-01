3ac91bd272006705f57f613f313945f09a86e68a
yarko35 <yarkinokular@gmail.com>
2021-01-02 02:25:33 +0300

    Yes Cheeef

--- Final/Fonksiyon bulma/notOrtalamaHesaplayici.m
+++ Final/Fonksiyon bulma/notOrtalamaHesaplayici.m
@@ -1,17 +1,18 @@
 clc;clear;clear All;
 while true
-   ortalamaHesapla=input('Normal ortalam için 1 i 50-50 için 2 i tuþlayýnýz,çýkmak için  q a basýnýz: ');
+   ortalamaHesapla=input('Normal ortalam için 1 i 50-50 için 2 i tuþlayýnýz,çýkmak için  3 e basýnýz: ');
 if ortalamaHesapla == 1
     vize=input('Vize notunuzu giriniz: ');
     final=input('Final notunuzu giriniz: ');
     notOrtalama=normal(vize,final);
-    fprintf('Dönem sonu ortalamanýz: %d',notOrtalama);
+    fprintf('Dönem sonu ortalamanýz: %d \n',notOrtalama);
     elseif ortalamaHesapla == 2
     vize=input('Vize notunuzu giriniz: ');
     final=input('Final notunuzu giriniz: ');
     notOrtalama=yuzdeelli(vize,final);
-    fprintf('Dönem sonu ortalamanýz: %d',notOrtalama);
-elseif ortalamaHesapla == 'q'
+    fprintf('Dönem sonu ortalamanýz: %d \n',notOrtalama);
+else 
+    disp('Çýkýþ yapýlýyor...')
     break;
 end
 end
