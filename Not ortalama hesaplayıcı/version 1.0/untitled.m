6f5e3252bd7a55649072b9f7e4fc7fd784899d91
yarko35 <yarkinokular@gmail.com>
2021-03-05 15:44:58 +0300

    version 1.0 added

---
+++ Not ortalama hesaplayıcı/version 1.0/untitled.m
@@ -0,0 +1,167 @@
+function varargout = untitled(varargin)
+% UNTITLED MATLAB code for untitled.fig
+%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED.M with the given input arguments.
+%
+%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled
+
+% Last Modified by GUIDE v2.5 05-Mar-2021 14:46:16
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled is made visible.
+function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled (see VARARGIN)
+
+% Choose default command line output for untitled
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in hesapla.
+function hesapla_Callback(hObject, eventdata, handles)
+%Notlarýn alýnmasý ve double a dönüþtürülmesi
+vize=str2double(get(handles.vize,'String'))
+final=str2double(get(handles.final,'String'))
+kirkatmis=get(handles.kirk,'Value')
+yirmiseksen=get(handles.yirmi,'Value')
+elli=get(handles.elli,'Value')
+if kirkatmis==1
+    totalnot=vize*0.4+final*0,6
+    set(handles.rakamnotu,'String',totalnot)
+elseif yirmiseksen ==1
+    totalnot=vize*0.2+final*0.8
+    set(handles.rakamnotu,'String',totalnot)
+elseif elli==1
+    totalnot=vize*0.5+final*0.5
+    set(handles.rakamnotu,'String',totalnot)
+end
+
+
+
+
+
+function vize_Callback(hObject, eventdata, handles)
+% hObject    handle to vize (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of vize as text
+%        str2double(get(hObject,'String')) returns contents of vize as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function vize_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to vize (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function final_Callback(hObject, eventdata, handles)
+% hObject    handle to final (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of final as text
+%        str2double(get(hObject,'String')) returns contents of final as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function final_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to final (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in kirk.
+function kirk_Callback(hObject, eventdata, handles)
+
+
+
+% Hint: get(hObject,'Value') returns toggle state of kirk
+
+
+% --- Executes on button press in yirmi.
+function yirmi_Callback(hObject, eventdata, handles)
+% hObject    handle to yirmi (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of yirmi
+
+
+% --- Executes on button press in elli.
+function elli_Callback(hObject, eventdata, handles)
+% hObject    handle to elli (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of elli
